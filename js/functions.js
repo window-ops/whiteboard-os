274430704849a9d8d9f4ff681a227f0cb58e291d
window-ops <vladbruma2020@gmail.com>
2023-06-28 17:20:21 +0300

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -66,21 +66,6 @@ function Navigate() {
   iframe.src = url;
 }
 
-
-function Navigate() {
-  let url = document.getElementById('URLBAR').value.trim();
-  const iframe = document.getElementById('FRAME');
-  iframe.addEventListener('load', function() {
-    document.getElementById('URLBAR').value = iframe.src;
-  });
-  
-  if (!url.startsWith('https://') && !url.startsWith('http://')) {
-    url = `https://${url}`;
-  }
-
-  iframe.src = url;
-}
-
 function ShutdownOptions() {
   document.getElementById("ShutdownOptionsMenu").style.display = "block";
   document.getElementById("Whiteboard").style.display = "none";
