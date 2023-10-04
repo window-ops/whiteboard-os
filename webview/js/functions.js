3f99751c8566b064a6a68de91fa5c34ec1bd5407
window-ops <vladbruma2020@gmail.com>
2023-10-04 14:46:04 +0000

    Update file functions.js

--- webview/js/functions.js
+++ webview/js/functions.js
@@ -1,15 +1,16 @@
+function Navigate() {
+  const iframe = document.getElementById('FRAME');
+  let url = document.getElementById('URLBAR').value.trim();
+  
+  if (!url.startsWith('https://') && !url.startsWith('http://')) {
+    url = `https://${url}`;
+  }
+
+  iframe.src = url;
+}
 window.onload = function() {
   const iframe = document.getElementById('FRAME');
   iframe.addEventListener('load', () => {
     document.getElementById('URLBAR').value = iframe.contentWindow.location.href;
   });
-  function Navigate() {
-    let url = document.getElementById('URLBAR').value.trim();
-    
-    if (!url.startsWith('https://') && !url.startsWith('http://')) {
-      url = `https://${url}`;
-    }
-
-    iframe.src = url;
-  }
 }
